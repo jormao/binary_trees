7dcc45fb5e208b94d2c7989c4bbc1f10156a5c7a
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-03-10 16:23:52 -0500

    Adds documentation support and adds some newlines

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,12 @@
 #include "binary_trees.h"
-#include <stdio.h>
-#include <stdlib.h>
 
+/**
+  * binary_tree_node - Creates a binary tree node.
+  * @parent: A pointer to the parent node of the node to create.
+  * @value: The value to put in the new node.
+  *
+  * Return: A pointer to the new node, or NULL on failure
+  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new_node;
