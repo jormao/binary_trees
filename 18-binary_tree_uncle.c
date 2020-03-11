f6a13a8b6de11bb2cbc5467c0e2c96c6f21cae11
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-03-11 16:54:44 -0500

    Reorder the conditions at stop of the recursion

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,12 +9,11 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent->parent == NULL ||
-		node->parent == NULL)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
-
 
 	if (node->parent->parent->right == node->parent)
 		return (node->parent->parent->left);
+
 	return (node->parent->parent->right);
 }
